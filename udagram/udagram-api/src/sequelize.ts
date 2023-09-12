f9f932bcdccd04c70366373a8c0ddfc69df999c0
Mohammed Almoghani <m.almoghani@outlook.com>
2023-09-12 17:33:47 +0300

    init

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -1,12 +1,18 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
-export const sequelize = new Sequelize({
-  username: config.username,
-  password: config.password,
-  database: config.database,
-  host: config.host,
-
-  dialect: "postgres",
-  storage: ":memory:",
-});
+export const sequelize = new Sequelize(
+  config.username,
+  config.password,
+  config.database,
+  {
+    host: config.host,
+    port: 5432,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }
+  }
+);
